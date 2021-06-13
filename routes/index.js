53bf19c8d5d04984b656709f3f716671e82d0d62
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-12 19:21:14 -0500

    Schema/api template

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+const Transaction = require("../models/transaction.js");
+
+router.post("/api/transaction", ({ body }, res) => {
+
+});
+
+
+module.exports = router;
