f92879e0fbbcdcc2e79d564136d31db7f94e8605
Christian Bryant <74256156+Christian-Bryant2020@users.noreply.github.com>
2021-06-13 01:06:31 -0500

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,46 +1,54 @@
-const router = require("express").Router();
-const Workout = require("../models/fitnessSchema.js");
-const { db } = require("../models/fitnessSchema.js");
+const router = require('express').Router();
+const { db } = require('../models/workout.js');
+const Workout = require('../models/workout.js');
+//post
+router.post('/api/workouts', (req , res) => {
+    //create new workout
+    console.log(req.body)
+    Workout.create(req.body)
+    .then((dbWorkout)=> {
+        res.json(dbWorkout);
+    })
+    .catch((err) => {
+        res.json(err);
+    });
+});
+//PUT add new exercises to plan
+router.put('/api/workouts/:id', (req, res) => {
+    const workoutId = req.params.id;
+    Workout.updateOne(
+        { _id: workoutId }, 
+        { $push: { exercises: req.body } },
+        function (error, success) {
+            if (error) {
+                res.status(400);
+                res.json(error);
+            } else {
+                res.json(success);
+            }
+        }
+    );
+});
 
-router.put("/api/workouts/:id",(req, res) => {
-    Workout.findByIdAndUpdate(req.params.id,body)
+//GET the combined weight of multiple exercises from the past seven workouts on the stats page
+router.get('/api/workouts', (req, res) => {
+    //one GET route with SWITCH statments
+    Workout.find(stats)
     .then(dbWorkout => {
         res.json(dbWorkout);
     })
     .catch(err => {
-      res.status(400).json(err);
+        res.json(err);
     });
-});
+})
 
-router.post("/api/workout", (req, res) => {
-  console.log(req.body)
-  Workout.create(req.body)
-    .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/workouts/range",(req,res) => {
-  Workout.find().limit(7)
-  .then(dbWorkout => {
-      res.json(dbWorkout);
-    })
-    .catch(err => {
-      res.status(400).json(err);
-    });
-});
-
-router.get("/api/workouts",(req,res) => {
-    Workout.find()
+router.delete('api/workouts', (req, res) => {
+    Workout.find(stats)
     .then(dbWorkout => {
         res.json(dbWorkout);
-      })
-      .catch(err => {
-        res.status(400).json(err);
-      });
-});
-
+    })
+    .catch(err => {
+        res.json(err);
+    });
+})
 module.exports = router;
